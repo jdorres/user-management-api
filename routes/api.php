b4cdc65d189eb6cf0714b09b944aa0ef3e0eebcf
Julio <juliodorres@gmail.com>
2024-07-06 21:41:15 -0300

    adicionada a base da authenticação jwt

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,28 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserAddressController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/users', [UserController::class, 'index']);
-Route::post('/users', [UserController::class, 'store']);
-Route::get('/users/{id}', [UserController::class, 'show']);
-Route::post('/users/{id}', [UserController::class, 'update']);
-Route::delete('/users/{id}', [UserController::class, 'delete']);
-Route::post('/users/{id}/address', [UserAddressController::class, 'store']);
+Route::group(['middleware' => 'api','prefix' => 'auth'], function ($router) {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+Route::group(['middleware' => 'auth:api'], function ($router) {
+    Route::group(['prefix' => 'auth'], function ($router) {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::post('/refresh', [AuthController::class, 'refresh']);
+        Route::post('/me', [AuthController::class, 'me']);
+    });
+
+    Route::get('/users', [UserController::class, 'index']);
+    Route::post('/users', [UserController::class, 'store']);
+    Route::get('/users/{id}', [UserController::class, 'show']);
+    Route::post('/users/{id}', [UserController::class, 'update']);
+    Route::delete('/users/{id}', [UserController::class, 'delete']);
+    Route::post('/users/{id}/address', [UserAddressController::class, 'store']);
+});
